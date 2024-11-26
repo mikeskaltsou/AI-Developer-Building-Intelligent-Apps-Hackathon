4c6e15587b0eceb1d839e6aff37456dad16b2b86
Michalis Skaltsounakis <mi.skal2467@gmail.com>
2024-11-26 14:57:18 +0200

    Initial Commit

---
+++ azure-openai-sdk-hackathon/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string endpoint = "<Add AOAI GPT Enpoint>";
+string deploymentName = "<Add AOAI GPT Key>";
+string openAiApiKey = "<Add AOAI GPT Deployment name>";
+
+string searchEndpoint = "<Add Azure Search Enpoint>";
+string searchIndex = "<Add Azure Search Key>";
+string searchApiKey = "<Add Azure Search Index for eCommerce products>";
